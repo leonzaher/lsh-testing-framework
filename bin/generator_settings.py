3fb150e77956114c89f01a97c545483c9d15be1e
Leon <zaher.leon@gmail.com>
2018-01-23 21:50:22 +0100

    Refactored generator settings

--- bin/generator_settings.py
+++ bin/generator_settings.py
@@ -7,26 +7,26 @@ class GeneratorSettings(object):
     """
 
     def __init__(self):
-        self.mistake_distribution: AbstractDistribution = None
+        self.mistake_count_distribution: AbstractDistribution = None
         self.length_distribution: AbstractDistribution = None
         self.duplicates_count_distribution: AbstractDistribution = None
         self.numbers_length_distribution: AbstractDistribution = None
 
         self.operators_list: list = None
 
-        self.default_string_delimiter = " "
-        self.default_line_delimiter = "\n"
+        self.default_string_delimiter = "\n"
+        self.default_line_delimiter = "\n\n"
 
-    def set_mistake_distribution(self, distribution: AbstractDistribution):
-        self.mistake_distribution = distribution
+    def set_mistake_count_distribution(self, distribution: AbstractDistribution):
+        self.mistake_count_distribution = distribution
 
     def set_length_distribution(self, distribution: AbstractDistribution):
         self.length_distribution = distribution
 
-    def set_duplicates_distribution(self, distribution: AbstractDistribution):
+    def set_duplicates_count_distribution(self, distribution: AbstractDistribution):
         self.duplicates_count_distribution = distribution
 
-    def set_numbers_distribution(self, distribution: AbstractDistribution):
+    def set_numbers_length_distribution(self, distribution: AbstractDistribution):
         self.numbers_length_distribution = distribution
 
     def set_operators_list(self, operators_list: list):
